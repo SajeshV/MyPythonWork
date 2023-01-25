c3bf11d2e90c0618b06bbc4de7afa93366cb6d6f
SajeshV <sajesh.kumar@gmail.com>
2023-01-25 23:17:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def add(Var1, Var2):
+    return Var1 + Var2
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    result=add(1, 2)
+    print (result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
